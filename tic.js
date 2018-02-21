04a4036a6c15f5f4f888172ab5d1442230e57b03
BDFOZ <Ben.Faus@live.com>
2018-02-20 21:27:32 -0600

    Put in winner determination logic

--- tic.js
+++ tic.js
@@ -7,48 +7,54 @@ xBoxes = [];
 oBoxes = [];
 winSeq = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[3,6,9],[4,5,6],[7,8,9]]
 
-addPlay(xTurn);
+start();
 
-//puts an X in a grid box if clicked and the box is empty
-		//xTurn variable toggles to change to/from x/o turn
-function addPlay(xTurn){
+//adds click event listener to activate playerBoxes()
+function start(){
 	box.forEach(function(element){
-		element.addEventListener('click',function(){
-			if (this.textContent === ''){ //if clicked box textContent ===''
-				if (xTurn){
-					this.textContent = "X";		
-					xTurn = false;
-					xBoxes.push(this.id);
-				} else {
-					this.textContent = "O";
-					xTurn = true;
-					oBoxes.push(this.id);
-				}
-			console.log(this.id)
-			}
-		})
+		element.addEventListener('click',playerBoxes)
 	});
 }
 
-function checkWinner(){
-	for (var i = 0; i < winSeq.length; i++){
+function playerBoxes(){
+	if (this.textContent === ''){ //if clicked box textContent ===''
+		if (xTurn){
+			this.textContent = "X";		
+			xTurn = false;
+			xBoxes.push(Number(this.id));
+			if (xBoxes.length > 2){
+				checkWinner(xBoxes);
+			}
+		} else {
+			this.textContent = "O";
+			xTurn = true;
+			oBoxes.push(Number(this.id));
+			if (oBoxes.length > 2){
+				checkWinner(oBoxes);
+			}
+		}
+	console.log(this.id)
+	}
+}
+
+//compares current player sequence (xBoxes array / oBoxes array) against winning sequences (winSeq array)
+function checkWinner(player){
+	winSeq.forEach(function(element){
 		x = 0;
-		if(x)
-
-	}
-	noWinner = false;
+		y = 0;
+		player.forEach(function(check){
+			if(element.includes(check)){
+			x++;
+			if (x===3){
+				alert("Winner is " + document.getElementById(player[0]).textContent);
+				noWinner = false;
+				//removes click event listener from boxes to prevent further play
+				box.forEach(function(element){
+					element.removeEventListener('click',playerBoxes)
+					});
+				}
+			}
+		})
+	x = 0;
+	})
 }
-
-
-// game logic here
-function game(){
-	while(noWinner){
-		addPlay(xTurn);
-		checkWinner();
-	}
-}
-
-//logic for game
-//x or o turn
-	//determine using xTurn boolean
-//
