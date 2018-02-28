e908cff9adca53af84d5f91665b760a87974fa5a
BDFOZ <Ben.Faus@live.com>
2018-02-27 22:23:03 -0600

    added beginnings of play v computer functionality. Computer can successfully block game winning moves but is pretty glitchy. Computer also (currently), always goes second (as O), makes random selections when X is not threatening a win.

--- tic.js
+++ tic.js
@@ -5,6 +5,8 @@ var available = box;
 var noWinner = true; //variable used to determine if winner
 var player = document.getElementById("player");
 var comp = document.getElementById("comp");
+var compPlayer = false;
+var potentialWinningCombo;
 
 xBoxes = [];
 oBoxes = [];
@@ -12,15 +14,16 @@ winSeq = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[3,6,9],[4,5,6],[7,8,9]]; //re
 firstMoveNotMid = [1,3,7,9]; //if middle spot is taken, pick a corner for first move.
 
 player.addEventListener('click',start);
+comp.addEventListener('click',function(){
+	compPlayer = true; ///integrate this with playerBoxes somehow?
+	start();
+});
 
-function compGame(){
-
-}
 
 //adds click event listener to activate playerBoxes()
 function start(){
 	box.forEach(function(element){
-		element.addEventListener('click',playerBoxes)
+		element.addEventListener('click',playerBoxes);
 	});
 }
 
@@ -34,6 +37,9 @@ function playerBoxes(){
 			xBoxes.push(Number(this.id));
 			if (xBoxes.length > 2){
 				checkWinner(xBoxes);
+			}
+			if (compPlayer){
+				findNextPlay();
 			}
 		} else {
 			this.textContent = "O";
@@ -49,25 +55,32 @@ function playerBoxes(){
 
 //compares current player sequence (xBoxes array / oBoxes array) against winning sequences (winSeq array)
 	//if there's a match, winner is alerted and click event listners are removed
-function checkWinner(player){
+function checkWinner(boxesOwned){
+	var almost = false;
 	winSeq.forEach(function(element){
-		x = 0;
-		y = 0;
-		player.forEach(function(check){
+		var x = 0;
+		boxesOwned.forEach(function(check){
 			if(element.includes(check)){
-			x++;
-			if (x===3){
-				alert("Winner is " + document.getElementById(player[0]).textContent);
-				noWinner = false;
-				//removes click event listener from boxes to prevent further play
-				box.forEach(function(element){
-					element.removeEventListener('click',playerBoxes)
+				x++;
+				if (x===2){
+					potentialWinningCombo = element;
+					almost = true;
+				}
+				else if (x===3){
+					alert("Winner is " + document.getElementById(boxesOwned[0]).textContent);
+					noWinner = false;
+					//removes click event listener from boxes to prevent further play
+					box.forEach(function(element){
+						element.removeEventListener('click',playerBoxes)
 					});
 				}
 			}
 		})
 	x = 0;
 	})
+	if (almost){
+		return true;
+	}
 }
 
 //returns whether a square is available or not.
@@ -89,6 +102,28 @@ function findFirstPlay(){
 		var play = String(firstMoveNotMid[select]);
 		document.getElementById(play).textContent = "O";
 
+	}
+}
+
+//computer finds next play and click is simulated.
+function findNextPlay(){
+	var i = 0 ;
+	console.log(checkWinner(xBoxes));
+	if (checkWinner(xBoxes)){
+		console.log("close is " + potentialWinningCombo);
+		potentialWinningCombo.forEach(function(el){
+			if (xBoxes.indexOf(el) === -1 && isAvailable(el)){
+				document.getElementById(el).click();
+			}
+		})
+	} else {
+			while (i === 0){
+				var select = Math.floor(Math.random() * 9)+1;
+				if (isAvailable(select)){
+					document.getElementById(select).click();
+					i++;
+				}
+		}
 	}
 }
 
